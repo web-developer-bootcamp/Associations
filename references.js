3d80dc57dc446fe0d36108f31022aaf3d33504f2
Yintong <zhouyintong96@gmail.com>
2019-10-14 16:26:38 +0200

    update

--- references.js
+++ references.js
@@ -18,29 +18,37 @@ var userScherma = new mongoose.Schema({
 
 var User = mongoose.model("User", userScherma);
 
-Post.create({
-    title: 'How to cook the best burger pt2',
-    content: 'BLA BLA BLA BLA'
-}, function (err, post) {
-    if (err) {
+User.findOne({email:"bob@gmail.com"}).populate("posts").exec(function(err, user){
+    if(err){
         console.log(err);
-    } else {
-        User.findOne({ email: 'bob@gmail.com' }, function (err, foundUser) {
-            if (err) {
-                console.log(err)
-            } else {
-                foundUser.posts.push(post)
-                foundUser.save(function(err, data){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        console.log(data);
-                    }
-                })
-            }
-        });
+    }else{
+        console.log(user);
     }
 });
+
+// Post.create({
+//     title: 'How to cook the best burger pt3',
+//     content: 'blweisnvowbvuibwcowen'
+// }, function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         User.findOne({ email: 'bob@gmail.com' }, function (err, foundUser) {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 foundUser.posts.push(post)
+//                 foundUser.save(function(err, data){
+//                     if(err){
+//                         console.log(err);
+//                     }else{
+//                         console.log(data);
+//                     }
+//                 })
+//             }
+//         });
+//     }
+// });
 
 // User.create({
 //     email:'bob@gmail.com',
